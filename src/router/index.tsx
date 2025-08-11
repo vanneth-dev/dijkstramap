3f77e8e5ce13286881d95471e2302a125e7e055b
vannethtak <vannethtak03@gmail.com>
2025-08-11 18:58:41 +0700

    fix

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,14 +1,12 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import NotFound from '../components/NotFound';
-import Dijkstra from '../pages/Dijkstra';
-import MinimalDijkstra from '../pages/MinimalDijkstra';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NotFound from "../components/NotFound";
+import Dijkstra from "../pages/Dijkstra";
 
 function RouterApp() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Dijkstra />} />
-        <Route path="/d" element={<MinimalDijkstra />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
